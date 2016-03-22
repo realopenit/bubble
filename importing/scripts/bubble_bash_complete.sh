7ba8434b73c5bfb4d3ab5206b628cbcea3adb6c5
RealopenIT Bubble <bubble@realopenit.nl>
2016-03-22 14:28:22 +0100

    remove importing

--- importing/scripts/bubble_bash_complete.sh
+++
@@ -1,8 +0,0 @@
-#http://click.pocoo.org/5/bashcomplete/
-#http://click.pocoo.org/5/bashcomplete/#activation-script
-#And then you would put this into your bashrc instead:
-#
-# . /path/to/bubble_bash_complete.sh
-
-
-eval "$(_BUBBLE_COMPLETE=source bubble)"
